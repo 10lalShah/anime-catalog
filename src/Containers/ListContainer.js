dcf6fc4dc3cd7dd91af46c932bec7f92c78412fb
Bilal Shah <bilal@socar.my>
2022-09-10 21:48:37 +0800

    feat: pagination, search, status lists

--- src/Containers/ListContainer.js
+++ src/Containers/ListContainer.js
@@ -1,22 +1,47 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import AnimeCard from '@/Components/AnimeCard'
+import { Spinner } from '@ui-kitten/components'
+import { Icon, Input, Text } from '@ui-kitten/components'
+import { useDebounce } from '@/Hooks'
 
-const ListContainer = () => {
+const ListContainer = ({ navigation }) => {
+  const { index, routeNames } = navigation.getState()
   const [animeList, setAnimeList] = useState([])
+  const [page, setPage] = useState(1)
+  const [lastPage, setLastPage] = useState()
+  const [searchText, setSearchText] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const debouncedSearch = useDebounce(searchText, 500)
+
   const fetchAnime = async () => {
+    const showStatus = routeNames[index].toLowerCase()
     try {
-      const { data, status } = await axios.get(`https://api.jikan.moe/v4/anime`)
-      setAnimeList(data.data)
+      setLoading(true)
+      const { data, status } = await axios.get(
+        `https://api.jikan.moe/v4/anime?page=${page}&?status=${showStatus}&q=${debouncedSearch}`,
+      )
+      setLastPage(data.pagination.last_visible_page)
+      setAnimeList([...animeList, ...data.data])
+      setLoading(false)
     } catch (error) {
-      console.log('ERROR', error)
+      setLoading(false)
+      console.log('ERROR:', error)
     }
   }
 
   useEffect(() => {
-    fetchAnime()
-  }, [])
+    if (debouncedSearch) fetchAnime()
+  }, [page, navigation, debouncedSearch])
+
+  const loadMore = () => {
+    if (page < lastPage) {
+      setPage(page + 1)
+    }
+  }
+
 
   const renderAnimeList = ({ item }) => {
     const { score, year, title, images, rating } = item
@@ -30,10 +55,29 @@ const ListContainer = () => {
       />
     )
   }
-
+  const renderIcon = props => <Icon {...props} name={'search'} />
   return (
     <View>
-      <FlatList data={animeList} renderItem={renderAnimeList} />
+      <View style={{ marginHorizontal: 5, marginVertical: 10 }}>
+        <Input
+          value={searchText}
+          placeholder="Search Anime"
+          accessoryLeft={renderIcon}
+          onChangeText={nextValue => setSearchText(nextValue)}
+        />
+      </View>
+      <FlatList
+        data={animeList}
+        renderItem={renderAnimeList}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.8}
+        ListFooterComponent={loading && <Spinner size="giant" />}
+        ListFooterComponentStyle={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingVertical: 50,
+        }}
+      />
     </View>
   )
 }
