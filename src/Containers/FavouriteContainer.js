e4e53ebbf91696e4aa06cadff0ae1b9142a495b6
Bilal Shah <bilal@socar.my>
2022-09-11 21:27:27 +0800

    refactor: code clean up on fav screen, empty list comp

--- src/Containers/FavouriteContainer.js
+++ src/Containers/FavouriteContainer.js
@@ -1,15 +1,20 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
 import {
-  Icon,
-  Input,
-  TopNavigation,
-  TopNavigationAction,
-} from '@ui-kitten/components'
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+} from 'react-native'
+import React from 'react'
+import { Icon, TopNavigation, TopNavigationAction } from '@ui-kitten/components'
 import AnimeCard from '@/Components/AnimeCard'
+import { scale } from 'react-native-size-matters'
 import { useSelector } from 'react-redux'
+import { useTheme } from '@/Hooks'
 
 const FavouriteContainer = ({ navigation }) => {
+  const { Images } = useTheme()
   const favourites = useSelector(state => state.favourites)
 
   const BackIcon = props => <Icon {...props} name="menu-outline" />
@@ -27,6 +32,15 @@ const FavouriteContainer = ({ navigation }) => {
     )
   }
 
+  const RenderEmptyList = () => {
+    return (
+      <View style={styles.emptyListContainer}>
+        <Image source={Images.pikachu} style={{ height: 200, width: 200 }} />
+        <Text category="s1">Uh ohh, nothing here</Text>
+      </View>
+    )
+  }
+
   const BackAction = () => (
     <TopNavigationAction
       icon={BackIcon}
@@ -34,11 +48,27 @@ const FavouriteContainer = ({ navigation }) => {
     />
   )
   return (
-    <View style={{ flex: 1 }}>
+    <SafeAreaView style={styles.container}>
       <TopNavigation accessoryLeft={BackAction} title="Favourite Anime" />
-      <FlatList data={favourites} renderItem={renderAnimeList} />
-    </View>
+      <FlatList
+        data={favourites}
+        renderItem={renderAnimeList}
+        ListEmptyComponent={<RenderEmptyList />}
+      />
+    </SafeAreaView>
   )
 }
 
 export default FavouriteContainer
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  emptyListContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: scale(100),
+  },
+})
